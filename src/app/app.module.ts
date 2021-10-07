67c98e96831acdcb828aac7daeffa98c7a3549eb
SamyrOR <saamyr@live.com>
2021-10-06 23:33:00 -0300

    feat: fix theme change and mobile button

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { RegularModule } from './regular/regular.module';
+import { CertificatesComponent } from './certificates/certificates.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { AboutMeComponent } from './about-me/about-me.component';
+import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, CertificatesComponent, ProjectsComponent, AboutMeComponent, ContactComponent],
   imports: [RegularModule, BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
--- src/app/regular/header/header.component.ts
+++ src/app/regular/header/header.component.ts
@@ -3,13 +3,53 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent implements OnInit {
-
-  constructor() { }
+  isDarkTheme: boolean = false;
+  isMenuOpen: boolean = false;
+  logos!: Element[];
+  constructor() {}
 
   ngOnInit(): void {
+    this.isDarkTheme =
+      localStorage.getItem('selected-theme') === 'dark' ? true : false;
+    this.switchTheme();
   }
 
+  switchTheme() {
+    this.logos = Array.from(document.querySelectorAll('.logo'));
+    if (this.isDarkTheme) {
+      this.transition();
+      document.documentElement.classList.remove('light-theme');
+      document.documentElement.classList.add('dark-theme');
+      this.logos.forEach((logo) =>
+        logo.setAttribute('src', './assets/images/dark-logo.png')
+      );
+    } else {
+      this.transition();
+      document.documentElement.classList.add('light-theme');
+      document.documentElement.classList.remove('dark-theme');
+      this.logos.forEach((logo) =>
+        logo.setAttribute('src', './assets/images/logo.png')
+      );
+    }
+    localStorage.setItem('selected-theme', this.isDarkTheme ? 'dark' : 'light');
+  }
+
+  transition() {
+    document.documentElement.classList.add('transition');
+    window.setTimeout(() => {
+      document.documentElement.classList.remove('transition');
+    }, 1000);
+  }
+
+  menuOpen() {
+    if (window.innerWidth < 1024) {
+      this.isMenuOpen = !this.isMenuOpen;
+      this.isMenuOpen
+        ? document.body.classList.add('no-scroll')
+        : document.body.classList.remove('no-scroll');
+    }
+  }
 }
