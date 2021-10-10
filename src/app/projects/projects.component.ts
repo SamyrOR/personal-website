9bd98ddaaa91c14f5ffa9138716102afebef80e9
SamyrOR <saamyr@live.com>
2021-10-10 18:05:00 -0300

    feat: created projects page

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -1,15 +1,40 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-projects',
   templateUrl: './projects.component.html',
-  styleUrls: ['./projects.component.scss']
+  styleUrls: ['./projects.component.scss'],
 })
 export class ProjectsComponent implements OnInit {
+  @ViewChild('cards') cards!: ElementRef;
+  constructor() {}
 
-  constructor() { }
+  zindex: number = 10;
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  onClick(e: any) {
+    e.preventDefault();
+    let cards = this.cards.nativeElement;
+    let card = e.currentTarget;
+    let isShowing = false;
+    if (card.classList.contains('show')) {
+      isShowing = true;
+    }
+    if (cards.classList.contains('showing')) {
+      let showingCard = document.querySelector('div.card.show');
+      showingCard?.classList.remove('show');
+      if (isShowing) {
+        cards.classList.remove('showing');
+      } else {
+        card.style.zIndex = this.zindex;
+        card.classList.add('show');
+      }
+      this.zindex++;
+    } else {
+      cards.classList.add('showing');
+      card.style.zIndex = this.zindex;
+      card.classList.add('show');
+      this.zindex++;
+    }
   }
-
 }
