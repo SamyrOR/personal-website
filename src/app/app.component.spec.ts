605dd895a50794681e3eb826bbc9da0be959871e
SamyrOR <saamyr@live.com>
2021-11-14 00:23:55 -0300

    feat: done unit test at service and main component

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,19 +1,62 @@
-import { TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { Title } from '@angular/platform-browser';
+import { ActivatedRoute, NavigationEnd, Router } from '@angular/router';
 import { RouterTestingModule } from '@angular/router/testing';
+import { from, Observable, of } from 'rxjs';
 import { AppComponent } from './app.component';
 
+class MockRouter {
+  //Router
+  public ne = new NavigationEnd(
+    0,
+    'http://localhost:4200/home',
+    'http://localhost:4200/home'
+  );
+  public events = new Observable((observer) => {
+    observer.next(this.ne);
+    observer.complete();
+  });
+}
+
 describe('AppComponent', () => {
+  let fixture: ComponentFixture<AppComponent>;
+  let app: AppComponent;
+  let titleService: Title;
+  let router: Router;
+  let activatedRoute: ActivatedRoute;
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [RouterTestingModule],
       declarations: [AppComponent],
+      providers: [
+        { provide: Router, useClass: MockRouter },
+        {
+          provide: ActivatedRoute,
+          useValue: { firstChild: { snapshot: { data: { title: 'Home' } } } },
+        },
+        Title,
+      ],
     }).compileComponents();
+    fixture = TestBed.createComponent(AppComponent);
+    app = fixture.componentInstance;
+    titleService = TestBed.inject(Title);
+    activatedRoute = TestBed.inject(ActivatedRoute);
+    app.ngOnInit();
+    await fixture.whenStable();
   });
 
   it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
     expect(app).toBeTruthy();
+  });
+
+  it('should change tittle dynamically', () => {
+    let appTitle = titleService.getTitle();
+    expect(appTitle).toBe('>_DEV - Home');
+    let child = activatedRoute.firstChild;
+    child!.snapshot.data['title'] = 'Certificates';
+    fixture.detectChanges();
+    appTitle = titleService.getTitle();
+    expect(appTitle).toBe('>_DEV - Certificates');
   });
 
   // it(`should have as title 'personal-website'`, () => {
--- src/app/shared/get-data.service.spec.ts
+++ src/app/shared/get-data.service.spec.ts
@@ -6,11 +6,44 @@ import {
 } from '@angular/common/http/testing';
 
 import { GetDataService } from './get-data.service';
+import { Projects } from './project';
+import { Certificate } from '../certificates/models/certificate';
+
+let mockProjects: Projects[] = [
+  {
+    title: 'Full e-commerce',
+    img: '../../assets/images/projects/js-amazona.jpeg',
+    caption: 'Plataforma de e-commerce completa desenvolvida com vanilla JS',
+    description:
+      'Desenvolvido desde o front-end até o back end com JavaScript, tento todo processo de seleção e compra da mercadoria, empregando diversas tecnologias.',
+    link: 'https://github.com/SamyrOR/full-ecommerce',
+  },
+  {
+    title: 'Games Info',
+    img: '../../assets/images/projects/games-info.jpeg',
+    caption: 'Aplicação em Angular com consumo de uma API de jogos',
+    description:
+      'Plataforma para pesquisa de jogos, com detalhes do jogo selecionado, desenvolvida com as melhores práticas em Angular.',
+    link: 'https://github.com/SamyrOR/games-info',
+  },
+];
+
+let mockCertificates: Certificate[] = [
+  {
+    name: 'Digital Innovation One',
+    certificates: [
+      {
+        name: 'Lógica de programação essencial',
+        img: '../../assets/images/certificates/dio-logica.jpg',
+        link: 'https://certificates.digitalinnovation.one/035F6F4D',
+      },
+    ],
+  },
+];
 
 describe('GetDataService', () => {
   let service: GetDataService;
   let httpTestingController: HttpTestingController;
-  let httpClient: HttpClient;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
@@ -18,11 +51,34 @@ describe('GetDataService', () => {
       providers: [GetDataService, HttpClient],
     });
     service = TestBed.inject(GetDataService);
-    httpClient = TestBed.inject(HttpClient);
     httpTestingController = TestBed.inject(HttpTestingController);
+  });
+
+  afterEach(() => {
+    httpTestingController.verify();
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+  it('should get projects json', () => {
+    service.getProjects().subscribe((projectsData) => {
+      expect(projectsData[0].title).toBe('Full e-commerce');
+      expect(projectsData[1].title).toBe('Games Info');
+    });
+
+    const req = httpTestingController.expectOne('assets/data/projects.json');
+    req.flush(mockProjects);
+  });
+
+  it('should get certificates json', () => {
+    service.getCertificates().subscribe((certificatesData) => {
+      expect(certificatesData[0].name).toBe('Digital Innovation One');
+    });
+
+    const req = httpTestingController.expectOne(
+      'assets/data/certificates.json'
+    );
+    req.flush(mockCertificates);
   });
 });
