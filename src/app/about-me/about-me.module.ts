fdbec05d9870c11350356a5a30c4bc18863ad659
SamyrOR <saamyr@live.com>
2024-09-02 12:49:57 -0300

    feat(i18n): added translation for "About me" page

--- src/app/about-me/about-me.module.ts
+++ src/app/about-me/about-me.module.ts
@@ -2,9 +2,10 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { AboutMeRoutingModule } from './about-me-routing.module';
 import { AboutMeComponent } from './about-me.component';
+import { TranslateModule } from '@ngx-translate/core';
 
 @NgModule({
   declarations: [AboutMeComponent],
-  imports: [CommonModule, AboutMeRoutingModule],
+  imports: [CommonModule, AboutMeRoutingModule, TranslateModule.forChild()],
 })
 export class AboutMeModule {}
